9554d0658e0e204fd0cc6d1e02c47bacd00c3b7a
bereketzerihun <bekykirazzer@gmail.com>
2026-08-07 04:16:36 -0700

    commiting day 16 task

---
+++ module-02/day-16/js/script.js
@@ -0,0 +1,36 @@
+
+let bill = Number(prompt ("Enter the bill please:") )
+let partySize = Number(prompt ("Enter the party size please:")) 
+
+
+
+let PerPerson = 0
+
+let total = 0 
+
+if (bill > 300 ){
+  total = bill + 30
+}
+else {
+  total = bill + 15
+}
+PerPerson = total/partySize
+
+console.log (`bill:${bill} \n the party size is:${partySize} \n the total price is :${total} \n the per person share: ${PerPerson}`)
+
+payment_method = "telebirr"
+switch(payment_method){
+
+  case "telebirr":
+    console.log("you used telebirr for payment")
+    break;
+
+  case "cbe":
+    console.log("you used CBE for payment")
+    break;
+
+  default:
+    console.log("you used unknown method payment")
+    break;
+}
+
