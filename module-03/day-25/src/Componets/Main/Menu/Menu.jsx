93071ee00526f4c50a84036e7bcef9947ed6c3e0
BereketZerihun <101508541+BereketZerihun@users.noreply.github.com>
2026-08-28 04:14:57 -0700

    Update Menu.jsx

--- module-03/day-25/src/Componets/Main/Menu/Menu.jsx
+++ module-03/day-25/src/Componets/Main/Menu/Menu.jsx
@@ -5,8 +5,8 @@ function Menu (){
     { id: 1, name: 'Doro wot', price: 350 },
     { id: 2, name: 'ketifo', price: 400 },
     { id: 3, name: 'shiro', price: 150 },
-    { id: 3, name: 'beyayenetu', price: 150 },
-    { id: 3, name: 'mahberawi', price: 150 }
+    { id: 4, name: 'beyayenetu', price: 150 },
+    { id: 5, name: 'mahberawi', price: 150 }
 
 
   ]
