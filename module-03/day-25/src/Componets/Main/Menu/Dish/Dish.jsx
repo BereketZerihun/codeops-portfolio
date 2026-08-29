7c30cfcdfd3dd91f81d95084451f7f9d465d82d6
bereketzerihun <bekykirazzer@gmail.com>
2026-08-29 07:49:53 -0700

    State & Events — useState

--- module-03/day-25/src/Componets/Main/Menu/Dish/Dish.jsx
+++ module-03/day-25/src/Componets/Main/Menu/Dish/Dish.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useState } from 'react';
 
 function Dish({ name, price, currency = "Birr", spicy }) {
+  const [orders,orderState] = useState(0)
   return (
     <div>
+      <h2>{orders}</h2>
       <h3>
         {name} {Boolean(spicy) && <span>🌶️ Spicy</span>}
       </h3>
       <p>Price: {price} {currency}</p>
+      <button onClick={()=>orderState(orders+1)}><h2>Add</h2></button>
     </div>
   );
 }
